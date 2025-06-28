54dff46e90c2b1173a2a95dec01e536bc1fe40e6
Camila Diniz <ddevcamila@gmail.com>
2025-06-28 17:23:31 -0300

    Finalização trab faculdade

---
+++ script.js
@@ -0,0 +1,7 @@
+let botao = document.getElementById('btn');
+let frase = document.getElementById('frase-motivacional');
+
+botao.addEventListener('click', function() {
+    frase.textContent = 'Cada linha de código é um avanço!';
+});
+
